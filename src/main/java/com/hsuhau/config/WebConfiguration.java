c93ef24f542e969e89ac6ccdec95c07d1885ff90
hsuhau <hsuhau@foxmail.com>
2020-05-21 00:28:49 +0800

    第一部分，总览spring boot

---
+++ src/main/java/com/hsuhau/config/WebConfiguration.java
@@ -0,0 +1,56 @@
+package com.hsuhau.config;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+//@SpringBootApplication
+
+/**
+ * 非@Configuration类WebConfiguration在当前项目行为上与改造前无异，
+ * 说明SpringApplication#run(Class,String...)方法引导Springboot应用时，
+ * 并不强依赖于@Configuration类作为首参，该参数称为“首要配置源”--primarySource，
+ * 更深层次的讨论请参考
+ */
+//@EnableAutoConfiguration
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public RouterFunction<ServerResponse> helloWorld() {
+        return route(GET("/hello-world"), request -> ok().body(Mono.just("Hello,world"), String.class));
+    }
+
+//    @Bean
+//    public ApplicationRunner runner(WebServerApplicationContext context) {
+//        return args -> {
+//            System.out.println("当前WebServer实现类为: "
+//                    + context.getWebServer().getClass().getName());
+//        };
+//    }
+
+    @Bean
+    public ApplicationRunner runner(BeanFactory beanFactory) {
+        return args -> {
+            System.out.println("当前helloworld实现类为: "
+                    + beanFactory.getBean("helloWorld").getClass().getName());
+            System.out.println("当前WebConfiguration Bean实现类为"
+                    + beanFactory.getBean(WebConfiguration.class).getClass().getName());
+        };
+    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("当前WebServer实现类为： " +
+                event.getWebServer().getClass().getName());
+    }
+}
---
+++ src/test/java/com/hsuhau/firstappbygui/FirstAppByGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsuhau.firstappbygui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppByGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
